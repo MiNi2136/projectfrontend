f7be8d56fa4422ae69e507c4d13b6457429b89a0
MiNi2136 <rimihaque119280@gmail.com>
2025-08-29 11:44:58 +0600

    Remove tutorial from landing page - show login/register directly

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,40 +2,26 @@ import React from "react";
 import { useEffect } from "react";
 import "../styles/Landing.css";
 import { Link } from "react-router-dom";
-import About from "./About";
 
 const Landing = () => {
-  const [Tutorial, setTutorial] = React.useState(
-    localStorage.getItem("tutorial") ? false : true
-  );
-
   useEffect(() => {
     if (localStorage.getItem("token")) {
       window.location.href = "/login";
     }
   });
 
-  function toggleDone() {
-    setTutorial(false);
-    localStorage.setItem("tutorial", false);
-  }
-
   return (
     <div className="landing-main">
-      {Tutorial ? (
-        <About toggleDone={toggleDone} />
-      ) : (
-        <div className="landing-main">
-          <h1>Qr Code Attendance System</h1>
-          <p>Hello and welcome!</p>
-          <Link to="/login" className="landing-login-button">
-            Login
-          </Link>
-          <Link to="/register" className="landing-register-button">
-            Register
-          </Link>
-        </div>
-      )}
+      <div className="landing-main">
+        <h1>Qr Code Attendance System</h1>
+        <p>Hello and welcome!</p>
+        <Link to="/login" className="landing-login-button">
+          Login
+        </Link>
+        <Link to="/register" className="landing-register-button">
+          Register
+        </Link>
+      </div>
     </div>
   );
 };
